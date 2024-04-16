06156616cadb0bf6a90e26ecc5542aca889ff638
carodriguezp <crpzgz92@gmail.com>
2024-04-16 23:23:34 +0200

    App structure updated

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import logo from '../logo.svg';
-import '../App.css';
+
 import { getDataFromApi } from '../services/api';
 import type { GnomeType, GnomeTypeResponse } from '../components/common.types'
 import { getJobList } from './main/form/dropDown/helper';
 
+import Header from './Header';
+import Form from './main/form/Form';
+import Footer from './Footer';
+import GnomeList from './main/gnomes/GnomeList';
 
+import logo from '../logo.svg';
+import '../App.css';
 
 function App() {
 
@@ -18,20 +23,22 @@ function App() {
   }
 
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
-    const fetchApi = async () => {
-      await fetchDataFromApi()
-    };
-    fetchApi()
+
+    fetchDataFromApi()
 
   }, [])
 
 
   return (
-    <body className="App">
+    <main className="App">
 
-    </body>
+      <Header />
+      <Form />
+      <GnomeList />
+      <Footer />
+
+    </main>
   );
 }
 
