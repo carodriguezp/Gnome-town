5b4232e151668b82ff90dc036ef8ee6944936244
carodriguezp <crpzgz92@gmail.com>
2024-04-21 15:54:01 +0200

    component GnomeList updated

--- src/components/main/gnomes/GnomeList.tsx
+++ src/components/main/gnomes/GnomeList.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+import { getDataFromApi } from '../../../services/api'
+import GnomeCard from './GnomeCard';
+import type { GnomeType, GnomeTypeResponse } from '../../common.types'
 
-function GnomeList() {
+function GnomeList({ gnomes }: { gnomes: GnomeTypeResponse[] }) {
+
+
     return (
-        <div>
+        <section className="">
 
-        </div>
+            <h2 className="">List of Gnomes</h2>
+
+            <ul className="" >
+                {gnomes.map((gnome) => {
+
+
+                    return <li className="" key={gnome.id}>
+                        <Link to={`/gnome/${gnome.id}`}>
+                            {/* <CharacterCard character={character} /> */}
+
+                            <div>
+                                <img className="" src={gnome.image} alt={gnome.name} />
+                                <h3>Name: {gnome.name}</h3>
+                                <p>Age: {gnome.age}</p>
+                                <p>Works as: {gnome.job.length ? gnome.job.join(", ") : "Nothing"}</p>
+                            </div>
+
+                        </Link>
+                    </li>
+
+                })}
+            </ul>
+
+        </section>
     )
 }
+
+
+
 
 export default GnomeList
