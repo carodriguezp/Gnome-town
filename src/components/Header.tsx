942098868a3ccf7c5043f6187c4ebeb2259f3afd
carodriguezp <crpzgz92@gmail.com>
2024-04-23 20:07:04 +0200

    header style improved

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,19 @@ import { fonts, colors } from '../styles/variables';
 function Header() {
 
     const Header = styled.header`
+        position:sticky;
+        top: 0;
+        width: 100%;
+        height: 45px;
+        margin: 15px 0 15px 0;
+        background-color: ${colors.faireWood};
 
     h1{
+        margin: 0;
         color: ${colors.brownRed};
         font-family: ${fonts.titleFamily};
         text-align:center;
-        font-size: 2.5rem;
+        font-size: 2.2rem;
 
         @media all and (min-width: 768px) {
 
