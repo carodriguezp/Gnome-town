438629bf14e5361a263e4094edc7d373325864a0
carodriguezp <crpzgz92@gmail.com>
2024-04-23 18:57:47 +0200

    styled component added and common folders done

---
+++ src/styles/variables.ts
@@ -0,0 +1,32 @@
+///Fonts
+
+export const fonts = {
+    titleFamily: "IM Fell English SC",
+    titleWeight: "400"
+
+
+}
+
+//Colors
+
+export const colors = {
+    //brown reds
+    brownRed: "#683E3E",
+    claireBrownRed: "#915757",
+    faireBrownRed: "#B07A7A",
+
+    //coffee
+    darkCoffee: "#654A27",
+    wood: "#B08245",
+    claireWood: "#C8A473",
+    faireWood: "#E2CFB6",
+
+    //forest
+    darkForest: "#325332",
+    forest: "#579157",
+    clarireForest: "#8FBC8F"
+
+
+
+
+}
