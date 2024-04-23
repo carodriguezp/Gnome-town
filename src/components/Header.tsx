80ed674dafe767ab1a9d5917f1126865a1ef413c
carodriguezp <crpzgz92@gmail.com>
2024-04-23 18:58:33 +0200

    header styled

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import styled from 'styled-components';
+import { fonts, colors } from '../styles/variables';
 
 function Header() {
+
+    const Header = styled.header`
+
+    h1{
+        color: ${colors.brownRed};
+        font-family: ${fonts.titleFamily};
+        text-align:center;
+        font-size: 2.5rem;
+
+        @media all and (min-width: 768px) {
+
+        font-size: 3.5rem;
+
+        }
+    }
+    `;
+
     return (
-        <header>
+        <Header>
             <h1>Brastlewark</h1>
-        </header>
+        </Header>
     )
 }
 
